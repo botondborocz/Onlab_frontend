bff81b7e99cd6c7765d64e9586f92d183f0a31da
botondborocz <boroczboti17@gmail.com>
2024-04-24 18:29:10 +0200

    Changes in personal data

--- src/app/adminpersonaldata/adminpersonaldata.component.ts
+++ src/app/adminpersonaldata/adminpersonaldata.component.ts
@@ -1,119 +1,129 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { LOCALSTORAGE_TOKEN_KEY, LOCALSTORAGE_TYPE_KEY } from '../app.component';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import {MatDatepicker, MatDatepickerModule} from '@angular/material/datepicker';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {LOCALSTORAGE_TOKEN_KEY} from '../app.component';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatIconModule} from '@angular/material/icon';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import {Router} from '@angular/router';
 
 export interface Profile {
-  id:number
-  username:string;
-  password:string;
-  firstName:string;
-  lastName:string;
+  id: number
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
   //dateOfBirth:Date;
 }
+
 @Component({
   selector: 'app-adminpersonaldata',
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatIconModule],
+  imports: [MatCardModule, MatFormFieldModule,
+    FormsModule, ReactiveFormsModule, MatInputModule,
+    MatDatepickerModule, MatNativeDateModule,
+    MatIconModule],
   templateUrl: './adminpersonaldata.component.html',
   styleUrl: './adminpersonaldata.component.scss'
 })
 export class AdminpersonaldataComponent {
   hide = true;
-  oldUsername:string="";
-  oldPassword:string="";
-  oldFirstName:string=""; 
-  oldLastName:string="";
-  id:number=0;
+  oldUsername: string = "";
+  oldPassword: string = "";
+  oldFirstName: string = "";
+  oldLastName: string = "";
+  id: number = 0;
   //oldBirthDate:Date=new Date(2023,1,4);
 
-  url:string='';
-  username:string | null=''
+  url: string = '';
+  username: string | null = ''
 
-  constructor(private http:HttpClient, private snackBar:MatSnackBar, private formBuilder:FormBuilder, private router:Router){
-    this.url="/api/admin/personaldata";
-    this.username=localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+  constructor(private http: HttpClient, private snackBar: MatSnackBar, private formBuilder: FormBuilder, private router: Router) {
+    this.url = "/api/admin/personaldata";
+    this.username = localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
     this.getDatafromBackend();
     this.setvalues();
   }
 
   profileForm = this.formBuilder.group({
-    username:this.oldUsername,
-    password:this.oldPassword,
-    firstName:this.oldFirstName,
-    lastName:this.oldLastName
-    //datepicker:Date
-  })
-
-  getDatafromBackend(){
-    var responseData: Profile;
-    this.http.get<Profile>(this.url + '/'+ this.username).
-    subscribe({
-      next: (next) =>  {responseData = next;}, 
-      error: (error) => console.log(error), 
-      complete: () => {
-        console.log(responseData); 
-        this.id=responseData.id;
-        this.oldUsername=responseData.username;
-        this.oldPassword=responseData.password;
-        this.oldFirstName=responseData.firstName;
-        this.oldLastName=responseData.lastName;}
-      });
-  }
-
-  setvalues(){
-    this.profileForm.setValue({
     username: this.oldUsername,
     password: this.oldPassword,
     firstName: this.oldFirstName,
-    lastName: this.oldLastName, 
-    //datepicker: this.oldBirthDate
+    lastName: this.oldLastName,
+    datepicker: Date
+  })
+
+  getDatafromBackend() {
+    var responseData: Profile;
+    this.http.get<Profile>(this.url + '/' + this.username).subscribe({
+      next: (next) => {
+        responseData = next;
+      },
+      error: (error) => console.log(error),
+      complete: () => {
+        console.log(responseData);
+        this.id = responseData.id;
+        this.oldUsername = responseData.username;
+        this.oldPassword = responseData.password;
+        this.oldFirstName = responseData.firstName;
+        this.oldLastName = responseData.lastName;
+      }
+    });
+  }
+
+  setvalues() {
+    this.profileForm.setValue({
+      username: this.oldUsername,
+      password: this.oldPassword,
+      firstName: this.oldFirstName,
+      lastName: this.oldLastName,
+      datepicker: null
     })
   }
 
-  gohome(){
+  gohome() {
     this.router.navigate(['/admin/home']);
   }
 
-  show(){
+  show() {
     this.getDatafromBackend();
     this.setvalues();
   }
 
 
-  save(){
+  save() {
     if (!this.profileForm.valid) {
       return;
-    } 
-    var profileData: Profile={
+    }
+    var profileData: Profile = {
       id: this.id,
-      username:this.profileForm.value.username!=null ? this.profileForm.value.username : this.oldUsername,
-      password:this.profileForm.value.password!=null ? this.profileForm.value.password : this.oldPassword,
-      firstName:this.profileForm.value.firstName !=null ? this.profileForm.value.firstName : this.oldFirstName,
-      lastName:this.profileForm.value.lastName !=null ? this.profileForm.value.lastName : this.oldLastName,
+      username: this.profileForm.value.username != null ? this.profileForm.value.username : this.oldUsername,
+      password: this.profileForm.value.password != null ? this.profileForm.value.password : this.oldPassword,
+      firstName: this.profileForm.value.firstName != null ? this.profileForm.value.firstName : this.oldFirstName,
+      lastName: this.profileForm.value.lastName != null ? this.profileForm.value.lastName : this.oldLastName,
       //dateOfBirth:this.profileForm.value.datepicker !=null ? this.profileForm.value.datepicker : this.oldBirthDate,
     }
-  console.log(profileData);
-   this.http.post<Profile>(this.url + '/'+ this.username, profileData).subscribe( {
-    next: (response) => console.log(response),
-      error: (error) =>{ 
+    console.log(profileData);
+    this.http.post<Profile>(this.url + '/' + this.username, profileData).subscribe({
+      next: (response) => console.log(response),
+      error: (error) => {
         (this.snackBar).open(
-        "Data modification failed", 'Close', {duration: 10000, horizontalPosition: 'center', verticalPosition: 'top'}
+          "Data modification failed", 'Close', {duration: 10000, horizontalPosition: 'center', verticalPosition: 'top'}
         );
       },
       complete: () => {
         (this.snackBar).open(
-          "Data modification successful", "Close", {duration: 5000, horizontalPosition: 'center', verticalPosition: 'top'}
+          "Data modification successful", "Close", {
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          }
         );
       }
-  });
+    });
   }
 }
--- src/app/userpersonaldata/userpersonaldata.component.ts
+++ src/app/userpersonaldata/userpersonaldata.component.ts
@@ -1,120 +1,166 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { LOCALSTORAGE_TOKEN_KEY, LOCALSTORAGE_TYPE_KEY } from '../app.component';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import {MatDatepicker, MatDatepickerModule} from '@angular/material/datepicker';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {LOCALSTORAGE_TOKEN_KEY} from '../app.component';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatIconModule} from '@angular/material/icon';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import {Router} from '@angular/router';
+import {MatTabGroup} from "@angular/material/tabs";
+import {NgClass} from "@angular/common";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {ThemeService} from "../theme.service";
+import {MatIconButton} from "@angular/material/button";
+import {MatToolbar, MatToolbarRow} from "@angular/material/toolbar";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {AuthService} from "../auth.service";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export interface Profile {
-  id:number
-  username:string;
-  password:string;
-  firstName:string;
-  lastName:string;
-  dateOfBirth:Date;
+  id: number
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
 }
+
 @Component({
   selector: 'app-userpersonaldata',
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatIconModule],
+  imports: [MatCardModule, MatFormFieldModule, FormsModule,
+    ReactiveFormsModule, MatInputModule, MatDatepickerModule,
+    MatNativeDateModule, MatIconModule, MatTabGroup, NgClass,
+    MatSlideToggle, MatIconButton, MatToolbar, MatToolbarRow,
+    MatMenu, MatMenuItem, MatMenuTrigger],
   templateUrl: './userpersonaldata.component.html',
-  styleUrl: './userpersonaldata.component.scss'
+  styleUrl: './userpersonaldata.component.scss',
+  providers: [provideAnimations()]
 })
+
 export class PersonaldataComponent {
+  isDarkMode: boolean;
   hide = true;
-  oldUsername:string="";
-  oldPassword:string="";
-  oldFirstName:string=""; 
-  oldLastName:string="";
-  id:number=0;
-  oldBirthDate:Date=new Date(2023,1,4);
+  oldUsername: string = "";
+  oldPassword: string = "";
+  oldFirstName: string = "";
+  oldLastName: string = "";
+  id: number = 0;
+  oldBirthDate: Date = new Date(2023, 1, 4);
 
-  url:string='';
-  username:string | null=''
+  url: string = '';
+  username: string | null = ''
 
-  constructor(private http:HttpClient, private snackBar:MatSnackBar, private formBuilder:FormBuilder, private router:Router){
-    this.url="/api/user/personaldata";
-    this.username=localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+  constructor(private themeService: ThemeService, private authService: AuthService,
+              private http: HttpClient, private snackBar: MatSnackBar,
+              private formBuilder: FormBuilder, private router: Router) {
+    this.url = "/api/user/personaldata";
+    this.username = localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+    this.isDarkMode = themeService.isDarkMode();
     this.getDatafromBackend();
     this.setvalues();
   }
 
   profileForm = this.formBuilder.group({
-    username:this.oldUsername,
-    password:this.oldPassword,
-    firstName:this.oldFirstName,
-    lastName:this.oldLastName,
-    datepicker:this.oldBirthDate
-  })
-
-  getDatafromBackend(){
-    var responseData: Profile;
-    this.http.get<Profile>(this.url + '/'+ this.username).
-    subscribe({
-      next: (next) =>  {responseData = next;}, 
-      error: (error) => console.log(error), 
-      complete: () => {
-        console.log(responseData); 
-        this.id=responseData.id;
-        this.oldUsername=responseData.username;
-        this.oldPassword=responseData.password;
-        this.oldFirstName=responseData.firstName;
-        this.oldLastName=responseData.lastName;}
-        //this.oldBirthDate=responseData.dateOfBirth;}
-      });
-  }
-
-  setvalues(){
-    this.profileForm.setValue({
     username: this.oldUsername,
     password: this.oldPassword,
     firstName: this.oldFirstName,
     lastName: this.oldLastName,
     datepicker: this.oldBirthDate
+  })
+
+  getDatafromBackend() {
+    var responseData: Profile;
+    this.http.get<Profile>(this.url + '/' + this.username).subscribe({
+      next: (next) => {
+        responseData = next;
+      },
+      error: (error) => console.log(error),
+      complete: () => {
+        console.log(responseData);
+        this.id = responseData.id;
+        this.oldUsername = responseData.username;
+        this.oldPassword = responseData.password;
+        this.oldFirstName = responseData.firstName;
+        this.oldLastName = responseData.lastName;
+      }
+      //this.oldBirthDate=responseData.dateOfBirth;}
+    });
+  }
+
+  setvalues() {
+    this.profileForm.setValue({
+      username: this.oldUsername,
+      password: this.oldPassword,
+      firstName: this.oldFirstName,
+      lastName: this.oldLastName,
+      datepicker: this.oldBirthDate
     })
   }
 
-  gohome(){
-    this.router.navigate(['/user/home']);
-  }
 
-  show(){
+  show() {
     this.getDatafromBackend();
     this.setvalues();
   }
 
 
-  save(){
+  save() {
     if (!this.profileForm.valid) {
       return;
-    } 
-    var profileData: Profile={
-      id: this.id,
-      username:this.profileForm.value.username!=null ? this.profileForm.value.username : this.oldUsername,
-      password:this.profileForm.value.password!=null ? this.profileForm.value.password : this.oldPassword,
-      firstName:this.profileForm.value.firstName !=null ? this.profileForm.value.firstName : this.oldFirstName,
-      lastName:this.profileForm.value.lastName !=null ? this.profileForm.value.lastName : this.oldLastName,
-      dateOfBirth:this.profileForm.value.datepicker !=null ? this.profileForm.value.datepicker : this.oldBirthDate,
     }
-  console.log(profileData);
-   this.http.post<Profile>(this.url + '/'+ this.username, profileData).subscribe( {
-    next: (response) => console.log(response),
-      error: (error) =>{ 
+    var profileData: Profile = {
+      id: this.id,
+      username: this.profileForm.value.username != null ? this.profileForm.value.username : this.oldUsername,
+      password: this.profileForm.value.password != null ? this.profileForm.value.password : this.oldPassword,
+      firstName: this.profileForm.value.firstName != null ? this.profileForm.value.firstName : this.oldFirstName,
+      lastName: this.profileForm.value.lastName != null ? this.profileForm.value.lastName : this.oldLastName,
+      dateOfBirth: this.profileForm.value.datepicker != null ? this.profileForm.value.datepicker : this.oldBirthDate,
+    }
+    console.log(profileData);
+    this.http.post<Profile>(this.url + '/' + this.username, profileData).subscribe({
+      next: (response) => console.log(response),
+      error: (error) => {
         (this.snackBar).open(
-        "Data modification failed", 'Close', {duration: 10000, horizontalPosition: 'center', verticalPosition: 'top'}
+          "Data modification failed", 'Close', {duration: 10000, horizontalPosition: 'center', verticalPosition: 'top'}
         );
       },
       complete: () => {
         (this.snackBar).open(
-          "Data modification successful", "Close", {duration: 5000, horizontalPosition: 'center', verticalPosition: 'top'}
+          "Data modification successful", "Close", {
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          }
         );
       }
-  });
+    });
+  }
+
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  gotofavs() {
+    this.router.navigate(['/user/favourites']);
+  }
+
+  gotomyaccount() {
+    this.router.navigate(['/user/personaldata']);
+  }
+
+  gohome() {
+    this.router.navigate(['/user/home']);
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    this.themeService.setDarkMode(this.isDarkMode);
   }
 }
