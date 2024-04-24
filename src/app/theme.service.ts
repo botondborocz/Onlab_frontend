a67c178e77073e8b4f68477a4b0ac05067513a00
botondborocz <boroczboti17@gmail.com>
2024-04-24 18:27:49 +0200

    Component for managing dark theme

---
+++ src/app/theme.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  constructor() {
+  }
+
+  private darkMode = false;
+
+  isDarkMode() {
+    return this.darkMode;
+  }
+
+  setDarkMode(isDarkMode: boolean) {
+    this.darkMode = isDarkMode;
+    if (isDarkMode) {
+      document.body.classList.add('dark-theme');
+      document.body.classList.remove('light-theme')
+    } else {
+      document.body.classList.add("light-theme")
+      document.body.classList.remove('dark-theme');
+    }
+    //this.comp1.toggleTheme();
+    //this.comp2.toggleTheme();
+  }
+}
