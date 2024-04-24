2a77f60504cff7f057604e20262425a80b26ac94
botondborocz <boroczboti17@gmail.com>
2024-04-24 18:27:25 +0200

    Changes in login and signup styles

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,66 +1,83 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-import {FormBuilder} from '@angular/forms';
-import {Validator} from '@angular/forms';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { Console, error } from 'console';
-import { HttpClient } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
-import { LoginResponse } from '../interfaces';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { LOCALSTORAGE_TOKEN_KEY, LOCALSTORAGE_TYPE_KEY } from '../app.component';
-import { AuthService } from '../auth.service';
+import {Component} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {Router, RouterModule} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {LoginResponse} from '../interfaces';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {LOCALSTORAGE_TOKEN_KEY, LOCALSTORAGE_TYPE_KEY} from '../app.component';
+import {AuthService} from '../auth.service';
+import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule, ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule, MatFormField, MatInput, MatLabel, MatIcon, MatIconButton, MatSuffix, MatCardTitle, MatCard, MatCardHeader, MatCardContent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  providers: [provideAnimations()]
 })
 export class LoginComponent {
-    username:FormControl=new FormControl('')
-    password:FormControl=new FormControl('')
-    isAdmin:FormControl=new FormControl('')
-    response:any
-    constructor(private snackBar:MatSnackBar,
-      private http: HttpClient,
-      private router: Router,
-      private authService: AuthService){}
-     login(){
-      let url:string
-      let routerurl:string
-      if(this.isAdmin.value){
-        url='/api/auth/login/admin'
-        routerurl='/admin/home'
-      }
-      else {
-        url='/api/auth/login/user'
-        routerurl='/user/home'
-      }
+  //username: FormControl = new FormControl('')
+  //password: FormControl = new FormControl('')
+  //isAdmin: FormControl = new FormControl('')
+  hide: boolean = true;
+  response: any
 
-      var body={
-          username: this.username.value,
-          password: this.password.value
-      }
-       this.http.post<LoginResponse>(url, body).subscribe({next: (response) => {
+  constructor(private snackBar: MatSnackBar,
+              private http: HttpClient,
+              private router: Router,
+              private authService: AuthService,
+              private formBuilder: FormBuilder) {
+  }
+
+  profileForm = this.formBuilder.group({
+    username: "",
+    password: "",
+    isAdmin: ""
+  })
+
+  login() {
+    let url: string
+    let routerurl: string
+    if (this.profileForm.value.isAdmin) {
+      url = '/api/auth/login/admin'
+      routerurl = '/admin/home'
+    } else {
+      url = '/api/auth/login/user'
+      routerurl = '/user/home'
+    }
+
+    var body = {
+      username: this.profileForm.value.username,
+      password: this.profileForm.value.password
+    }
+    this.http.post<LoginResponse>(url, body).subscribe({
+      next: (response) => {
         console.log(response)
         localStorage.setItem(LOCALSTORAGE_TOKEN_KEY, response.username);
         localStorage.setItem(LOCALSTORAGE_TYPE_KEY, response.type);
-        },
-       error: (error) =>{
+      },
+      error: (error) => {
         this.snackBar.open(
-          'Incorrect username or password', 'Close', {duration: 3000, horizontalPosition: 'center', verticalPosition: 'top'}
-          );
-        },
-       complete: () => {
+          'Incorrect username or password', 'Close', {
+            duration: 3000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          }
+        );
+      },
+      complete: () => {
         (this.snackBar).open(
           "Successful login", "Close", {duration: 3000, horizontalPosition: 'center', verticalPosition: 'top'}
         );
         this.router.navigate([routerurl]);
-       }
-   });
+      }
+    });
   }
 }
 
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,44 +1,61 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { AuthService } from '../auth.service';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import {Component} from '@angular/core';
+import {Router, RouterModule} from '@angular/router';
+import {AuthService} from '../auth.service';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterModule, ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule, MatCard, MatCardContent, MatCardHeader, MatCardTitle, MatFormField, MatInput, MatLabel, MatIcon, MatIconButton, MatSuffix],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
+  providers: [provideAnimations()]
 })
 export class SignupComponent {
-  username:FormControl=new FormControl('')
-  password:FormControl=new FormControl('')
-  isAdmin:FormControl=new FormControl('')
+  //username: FormControl = new FormControl('')
+  //password: FormControl = new FormControl('')
+  //isAdmin: FormControl = new FormControl('')
+  hide: boolean = true;
 
-  constructor(private authService:AuthService, 
-    private router: Router,
-    private snackBar:MatSnackBar){}
+  constructor(private authService: AuthService,
+              private router: Router,
+              private snackBar: MatSnackBar,
+              private formBuilder: FormBuilder) {
+  }
+
+  profileForm = this.formBuilder.group({
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    isAdmin: ""
+  })
 
 
-  signup(){
-    var body={
-      username: this.username.value,
-      password: this.password.value,
-      isAdmin:this.isAdmin.value
+  signup() {
+    var body = {
+      username: this.profileForm.value.username,
+      password: this.profileForm.value.password,
+      isAdmin: this.profileForm.value.isAdmin
     }
-    var url:string
-    if(this.isAdmin.value){
-      url='/api/auth/signup/admin'
-    }
-    else{
-      url='/api/auth/signup/user'
+    var url: string
+    if (this.profileForm.value.isAdmin) {
+      url = '/api/auth/signup/admin'
+    } else {
+      url = '/api/auth/signup/user'
     }
     this.authService.register(body, url).subscribe({
       next: (response) => console.log(response),
       error: (error) => (this.snackBar).open(
         "Valamilyen hiba történt", 'Bezárás', {duration: 10000, horizontalPosition: 'right', verticalPosition: 'top'}
-        ),
+      ),
       complete: () => {
         (this.snackBar).open(
           "Sikeres regisztráció", "Bezárás", {duration: 5000, horizontalPosition: 'right', verticalPosition: 'top'}
@@ -48,3 +65,4 @@ export class SignupComponent {
     });
   }
 }
+
